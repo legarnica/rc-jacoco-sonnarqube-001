da599b629269757058c0c297144a516652e29e9b
Luis Herrera <luis.herrera@tinet.cl>
2021-07-26 11:11:37 -0400

    se inicia el proyecto con spring web

---
+++ src/test/java/cl/lherrera/jacoco/JacocoApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.lherrera.jacoco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JacocoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
